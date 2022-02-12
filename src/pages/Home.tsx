e7de3c1482a406ad0b16ede5e9933b9dce322408
Michael Uzan <looply3@gmail.com>
2022-02-13 01:04:04 +0200

    scss to page not found

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ export const Home = () => {
     const loggedInUser: any = useSelector((state: RootState) => state.userModule.loggedInUser)
 
     return (
-        <section className="home tac flex direction-col align-center">
+        <section className="home flex direction-col align-center">
             <h1>Welcome To Todo App!</h1>
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkWC8RIYQczoGgcwlWhGonDyXl8bI0_xfB8w&usqp=CAU" alt="todos" />
             {!loggedInUser && <Link to="/login" >Login to start</Link>}
--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -2,8 +2,9 @@ import { Link } from 'react-router-dom'
 
 export const PageNotFound = () => {
     return (
-        <section className="page-not-found">
-            <h1>Page not found</h1>
+        <section className="page-not-found flex direction-col align-center">
+            <h1>Page not found!</h1>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRViCNeqRLgb1uH29iLY5skAe1fR-kAMLjHaA&usqp=CAU" alt="lost" />
             <Link to='/'>Go Home</Link>
         </section>
     )
