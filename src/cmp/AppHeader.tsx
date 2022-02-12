98e36d5497a93113b60b12fcbf26e322f6bf6567
Michael Uzan <looply3@gmail.com>
2022-02-12 23:08:14 +0200

    scss to header + aplay hamburger menu

--- src/cmp/AppHeader.tsx
+++ src/cmp/AppHeader.tsx
@@ -2,6 +2,8 @@ import { NavLink, useHistory } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../store'
 import { onLogout } from '../store/actions/userActions';
+import { useState } from 'react';
+import logo from '../assets/imgs/logo.jpg';
 
 export const AppHeader = () => {
 
@@ -9,10 +11,21 @@ export const AppHeader = () => {
     const dispatch = useDispatch()
     const loggedInUser: any = useSelector((state: RootState) => state.userModule.loggedInUser)
 
+    const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+    const onMenuClick = (): void => {
+        setIsMenuOpen(!isMenuOpen)
+    }
+
+    const getMenuClass = (): string => {
+        return isMenuOpen ? 'menu-open' : ''
+    }
+
     const onLogoutClick = async () => {
         try {
             await dispatch(onLogout())
             history.push('/')
+            onMenuClick()
         } catch (err) {
             console.log(err)
         }
@@ -23,15 +36,20 @@ export const AppHeader = () => {
     }
 
     return (
-        <header className="full">
-            <h3>Hello {loggedInUser ? loggedInUser.username : 'guest'}</h3>
-            <nav className="flex justify-center">
-                <button onClick={onGoBack}>Back</button>
-                {!loggedInUser && <NavLink activeClassName="active" exact to="/login" >Login</NavLink>}
-                <NavLink activeClassName="active" exact to="/todo-app" >Todo</NavLink>
-                {loggedInUser && <NavLink activeClassName="active" exact to="/dashboard" >Dashboard</NavLink>}
-                {loggedInUser && <NavLink onClick={onLogoutClick} activeClassName="active" exact to="/" >Logout</NavLink>}
-            </nav>
+        <header className={`full flex space-between align-center ${getMenuClass()}`}>
+            <div className="flex align-center space-between">
+                <img src={logo} alt="logo" />
+                <button className="back" onClick={onGoBack}>Back</button>
+                <h3>Hello {loggedInUser ? loggedInUser.username : 'guest'}</h3>
+                <nav className="flex">
+                    {!loggedInUser && <NavLink onClick={onMenuClick} activeClassName="active" exact to="/login" >Login</NavLink>}
+                    <NavLink onClick={onMenuClick} activeClassName="active" exact to="/todo-app" >Todo</NavLink>
+                    {loggedInUser && <NavLink onClick={onMenuClick} activeClassName="active" exact to="/dashboard" >Dashboard</NavLink>}
+                    {loggedInUser && <NavLink onClick={onLogoutClick} activeClassName="active" exact to="/" >Logout</NavLink>}
+                </nav>
+            </div>
+            <div className="screen" onClick={onMenuClick}></div>
+            <button className="btn-menu-toggle" onClick={onMenuClick}>☰</button>
         </header>
     )
 }
