0d0197c07441e3fc5d2fb8f0bed4773f98640948
Michael Uzan <looply3@gmail.com>
2022-02-12 22:35:53 +0200

    applay basic scss, and fonts

--- src/cmp/AppFooter.tsx
+++ src/cmp/AppFooter.tsx
@@ -2,6 +2,7 @@ export function AppFotter() {
     return (
         <footer className="footer full flex justify-center">
             <p>&copy; coffee rights</p>
+            <img src="https://www.careology.health/images/footer/logo-32.svg" alt="" />
         </footer>
     );
 }
